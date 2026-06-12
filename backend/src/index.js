791f7c54279d7bb56662b5291b715a2389fde5e6
Polsito <jhanpolpb840@gmail.com>
2026-06-11 20:20:06 -0500

    feat:conexion a PostgreSQL configurada

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
-require (dotenv).config();
+require('dotenv').config();
 const express = require("express");
+require("./config/database");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
