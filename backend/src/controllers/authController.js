f7760d3a90dc50e199ccd9603ba84dbb202f9478
Polsito <jhanpolpb840@gmail.com>
2026-06-15 22:20:37 -0500

    feat: primer try catch del controller

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,3 +1,16 @@
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const pool = require("../config/database");
+
+const register = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    const exists = await pool.query("SELECT id FROM users WHERE email = $1", [email]);
+    if (exists.rows.length > 0) {
+      return res.status(400).json({ error: "El email ya está registrado" });
+    }
+    }catch (err) {
+    res.status(500).json({ error: "Error en el registro" });
+    }
+}
