457f6f0f167713f77904571b6ccbf16f91732df5
Polsito <jhanpolpb840@gmail.com>
2026-06-15 22:24:12 -0500

    feat: conectar la ruta al app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require("express");
 require("./config/database");
 const productRoutes = require("./routes/productRoutes");
+const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -12,7 +13,9 @@ app.get("/",(req,res) => {
     res.json({
         message:"Bienvenido a KicksHub API"});
     });
+
 app.use("/api/products", productRoutes);
+app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en el puerto ${PORT}`);
