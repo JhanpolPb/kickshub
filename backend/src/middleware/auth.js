17be2c9b4ed6537d0edf89a10404cf9c36eb8361
Polsito <jhanpolpb840@gmail.com>
2026-06-15 22:14:40 -0500

    feat: auth middleware creado

--- backend/src/middleware/auth.js
+++ backend/src/middleware/auth.js
@@ -7,17 +7,16 @@ const authMiddleware = (req, res, next) => {
   if (!authHeader || !authHeader.startsWith("Bearer ") ){
     return res.status(401).json({ error: "Token no proporcionado"});
   }
-}
-
 const token = authHeader.split("")[1];
 
-try{
+    try{
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded;
-    
+    next();
 
-}catch{
+    }catch(err){
+    return res.status(401).json({ error: "Token invalido o expirado"})
+    }
+};
 
-}
-
-app.use(logger)
+module.exports = authMiddleware;
