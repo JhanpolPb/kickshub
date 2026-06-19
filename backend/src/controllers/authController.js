0e87d20bd9be7021bda18278255de87eb7e536f1
Polsito <jhanpolpb840@gmail.com>
2026-06-18 20:07:11 -0500

    feat: metodo hashed

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -10,6 +10,12 @@ const register = async (req, res) => {
     if (exists.rows.length > 0) {
       return res.status(400).json({ error: "El email ya está registrado" });
     }
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+        const result = await pool.query(
+      "INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING id, name, email, role",
+      [name, email, hashedPassword]
+    );
     }catch (err) {
     res.status(500).json({ error: "Error en el registro" });
     }
