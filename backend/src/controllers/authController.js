e4c12ee7d97c8918835c1682eff148cbc4a85e72
Polsito <jhanpolpb840@gmail.com>
2026-06-20 22:53:48 -0500

    feat: manejo de excepcion de errores

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -52,18 +52,9 @@ const register = async (req, res) => {
       res.json({
         user: {id: user.id, name: user.name, email: user.email, role:user.role},
         token
-      })
-
-
-
-
-      }catch{
-
-
-      }
+      });
+    }catch(err){
+        res.status(500).json({ error: "Error en el login"});
     }
+};
 
-    }catch (err) {
-    res.status(500).json({ error: "Error en el registro" });
-    }
-}
