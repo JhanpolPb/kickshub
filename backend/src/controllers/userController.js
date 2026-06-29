1f7f5f7453be7a6c84183cc6c8e31844d8b9b198
Polsito <jhanpolpb840@gmail.com>
2026-06-29 11:57:24 -0500

    feat: primera consulta del controlador

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -4,6 +4,10 @@ const bcrypt = require('bcryptjs');
 const getProfile= async (req, res) => {
     try{
 
+        const result = await pool.query( "SELECT id, name, email, role, created_at FROM users WHERE id = $1", 
+            [req.user.id]
+        );
+        res.json(result.rows[0]);
     }catch{
 
     }
