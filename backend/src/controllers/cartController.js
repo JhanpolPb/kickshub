fc75bb71b550724e4670f891a2db34ed222731dc
Polsito <jhanpolpb840@gmail.com>
2026-07-05 12:47:12 -0500

    feat: Funcion addToCart

--- backend/src/controllers/cartController.js
+++ backend/src/controllers/cartController.js
@@ -14,11 +14,23 @@ const getCart = async (req, res) => {
     }
 };
 const addToCart = async (req, res) => {
+    const { id_product, quantity} = req.body;
     try{
+        const exists = await pool.query("SELECT * FROM cart_items WHERE id_user = $1 AND id_product = $2",[req.user.id, id_product]);
+
+        if (exists.rows.length > 0){
+            const result = await pool.query("UPDATE cart_items SET quantity = quantity + $1 WHERE id_user = $2 AND id_product = $3 RETURNING *",
+            [quantity, req.user.id, id_product]);
+            res.json(result.rows[0]);
+        }else{
+            const result = await pool.query("INSERT INTO cart_items (id_user, id_product, quantity) VALUES ($1,$2,$3) RETURNING *",
+            [req.user.id, id_product, quantity]);
+            res.status(201).json(result.rows[0]);
+        }
+      
 
     }catch(err){
-
-
+        res.status(500).json({ error: "Error agregando al carrito"});
     }
 };
 
