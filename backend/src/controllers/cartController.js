31cac6f3a065238fcbac584045487ef2a59f460b
Polsito <jhanpolpb840@gmail.com>
2026-07-05 12:39:05 -0500

    feat: get cartController

--- backend/src/controllers/cartController.js
+++ backend/src/controllers/cartController.js
@@ -1,3 +1,16 @@
 const pool = require("../config/database");
 
+const getCart = async (req, res) => {
+    try {
+        const result = await pool.query(
+            "SELECT cart_items.*, products.name, products.price, products.image_url FROM cart_items JOIN products ON cart_items.id_product = products.id WHERE cart_items.id_user = $1",
+            [req.user.id]
+        );
+        res.json(result.rows);
+
+    }catch{
+        res.status(500).json({ error: "Error obteniendo carrito"});
+
+    }
+};
 
