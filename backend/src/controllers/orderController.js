9c87375ae20666e2bbc26d9318f4ded2ea24bfc8
Polsito <jhanpolpb840@gmail.com>
2026-07-06 20:43:24 -0500

    feat: getOrders

--- backend/src/controllers/orderController.js
+++ backend/src/controllers/orderController.js
@@ -2,8 +2,10 @@ const pool = require("../config/database");
 
 const getOrders = async (req,res) => {
     try{
+        const result = await pool.query ("SELECT * FROM orders WHERE id_user = $1 ORDER BY created_at DESC",[req.user.id]);
+        res.json(result.rows);
 
     }catch(err){
-
+        res.status(500).json({ error: "Error al obtener las ordenes" });
     }
 };
