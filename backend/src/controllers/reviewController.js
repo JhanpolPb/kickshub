6a419f0758c68cec48c9579be5c600d4c5c9cf54
Polsito <jhanpolpb840@gmail.com>
2026-07-07 19:21:49 -0500

    feat: primera peticion

--- backend/src/controllers/reviewController.js
+++ backend/src/controllers/reviewController.js
@@ -2,8 +2,9 @@ const pool = require('../config/datbase');
 
 const getReviews = async (req,res) => {
     try{
-
+        const result = await pool.query ("SELECT * FROM reviews WHERE id_product = $1", [req.params.id_product]);
+        res.json(result.rows);
     }catch(err){
-
+        res.status(500).json ({ error: "Error al obtener las reseñas"});
     }
 };
