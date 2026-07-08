4159c84403b474e0f9696bc2586eed7a0d1f822e
Polsito <jhanpolpb840@gmail.com>
2026-07-07 19:39:44 -0500

    feat; modulos

--- backend/src/controllers/reviewController.js
+++ backend/src/controllers/reviewController.js
@@ -2,6 +2,7 @@ const pool = require('../config/database');
 
 const getReviews = async (req,res) => {
     try{
+        
         const result = await pool.query ("SELECT * FROM reviews WHERE id_product = $1", [req.params.id]);
         res.json(result.rows);
     }catch(err){
@@ -11,9 +12,9 @@ const getReviews = async (req,res) => {
 
 const createReview = async (req,res) => {
     try{
-        const { id } = req.body;
+       const { id_product, calification, comment } = req.body;
         const result = await pool.query ("INSERT INTO reviews (id_user, id_product, calification, comment) VALUES ($1,$2,$3,$4) RETURNING *",
-        [idreq.user.id, id_product, rating, comment]);
+        [req.user.id, id_product, calification, comment]);
         res.status(201).json(result.rows[0]);
     }catch(err){
         res.status(500).json({ error: "Error al crear la reseña"});
@@ -23,8 +24,8 @@ const createReview = async (req,res) => {
 
 const deleteReview = async (req,res) => {
     try{
-        const {id} = req.params.id;
-        const result = await pool.query ("DELETE FROM reviews WHERE id_user = $1 and id_product = $2 RETURNING *", [id,req.user.id]);
+        const {id} = req.params;
+        const result = await pool.query ("DELETE FROM reviews WHERE id = $1 and id_user = $2 RETURNING *", [id,req.user.id]);
 
         if (result.rows.length === 0) {
         return res.status(404).json({ error: "Reseña no encontrada" });
@@ -33,4 +34,10 @@ const deleteReview = async (req,res) => {
   } catch (err) {
     res.status(500).json({ error: "Error eliminando reseña" });
   }
+};
+
+module.exports = {
+    getReviews,
+    createReview,
+    deleteReview
 };
