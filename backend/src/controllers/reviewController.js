987cc409a27a01e3c44037e17f2a5da6b1a5f575
Polsito <jhanpolpb840@gmail.com>
2026-07-07 19:26:44 -0500

    feat: CreateReview

--- backend/src/controllers/reviewController.js
+++ backend/src/controllers/reviewController.js
@@ -11,8 +11,12 @@ const getReviews = async (req,res) => {
 
 const createReview = async (req,res) => {
     try{
-
+        const { id_product, rating, comment } = req.body;
+        const result = await pool.query ("INSERR INTO reviews (id_user, id_product, rating, comment) VALUES ($1,$2,$3,$4) RETURNING *",
+        [req.user.id, id_product, rating, comment]);
+        res.status(201).json(result.rows[0]);
     }catch(err){
+        res.status(500).json({ error: "Error al crear la reseña"});
 
     }
 };
