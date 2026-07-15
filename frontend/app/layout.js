d1903d091a0460496364bdab0be6da48dbbc1c6e
Polsito <jhanpolpb840@gmail.com>
2026-07-14 20:58:54 -0500

    feat: layout

---
+++ frontend/app/layout.js
@@ -0,0 +1,19 @@
+import { AuthProvider } from "../src/context/authContext";
+import "./globals.css";
+
+export const metadata = {
+  title: "KicksHub",
+  description: "Tu tienda de sneakers de confianza",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="es">
+      <body>
+        <AuthProvider>
+          {children}
+        </AuthProvider>
+      </body>
+    </html>
+  );
+}
