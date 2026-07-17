8451fc982ce2400926b88d66aff4ff74605d5b17
Polsito <jhanpolpb840@gmail.com>
2026-07-16 21:22:21 -0500

    feat: new dependences

--- frontend/app/login/page.js
+++ frontend/app/login/page.js
@@ -1,3 +1,7 @@
-export function loginPage() {
-  return <h1>Iniciar Sesión</h1>
-}
+"use client";
+
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { useAuth } from "../../src/context/AuthContext";
+import { login as loginService } from "../../src/services/authService";
+import Link from "next/link";
