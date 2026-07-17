4d9fb8d3c130462046af3e3c0cf052367b8ef3c5
Polsito <jhanpolpb840@gmail.com>
2026-07-16 21:23:49 -0500

    feat: exportar funciones

--- frontend/app/login/page.js
+++ frontend/app/login/page.js
@@ -5,3 +5,11 @@ import { useRouter } from "next/navigation";
 import { useAuth } from "../../src/context/AuthContext";
 import { login as loginService } from "../../src/services/authService";
 import Link from "next/link";
+
+export default function LoginPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+  const router = useRouter();
+  const { loading, setLoading } = useState(false); 
+}
