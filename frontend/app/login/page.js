b055635e6821b8721c0bae4ae9793e94284b5807
Polsito <jhanpolpb840@gmail.com>
2026-07-16 21:29:24 -0500

    feat: estructure Handle

--- frontend/app/login/page.js
+++ frontend/app/login/page.js
@@ -10,10 +10,15 @@ export default function LoginPage() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState(null);
- 
-  const { loading, setLoading } = useState(false); 
+ const { loading, setLoading } = useState(false); 
 
   const { login } = useAuth ();
   const { router } = useRouter ();
+
+  const handleSubmit = async (e) => {
+    
+  }
+
+
 
 }
