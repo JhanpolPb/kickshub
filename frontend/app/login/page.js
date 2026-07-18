fd5795f0e04778bfc9bdbae4f97f5655bf347935
Polsito <jhanpolpb840@gmail.com>
2026-07-17 20:55:49 -0500

    fix: error

--- frontend/app/login/page.js
+++ frontend/app/login/page.js
@@ -6,7 +6,7 @@ import { useAuth } from "../../src/context/AuthContext";
 import { login as loginService } from "../../src/services/authService";
 import Link from "next/link";
 
-export default function LoginPage() {
+export default async function LoginPage() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState(null);
