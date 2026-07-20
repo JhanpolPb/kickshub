42c56e63baf315faac329f223577d88bdb7433ed
Polsito <jhanpolpb840@gmail.com>
2026-07-19 22:08:53 -0500

    chore: new dependence cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const cors = require ("cors");
 const express = require("express");
 require("./config/database");
 const productRoutes = require("./routes/productRoutes");
