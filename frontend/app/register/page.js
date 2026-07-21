4bfccd10acfcdd61f2cb891068a286ffbe7e9f52
Polsito <jhanpolpb840@gmail.com>
2026-07-20 22:08:58 -0500

    feat: funcion registro

--- frontend/app/register/page.js
+++ frontend/app/register/page.js
@@ -2,3 +2,9 @@
 import { useState } from "react";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
+
+export default function RegisterPage(){
+  const [name, setName] = useState ("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+}
