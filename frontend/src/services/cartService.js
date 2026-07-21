e27a4c57034bc235292f5ffa49c488677ec287f3
Polsito <jhanpolpb840@gmail.com>
2026-07-21 17:29:58 -0500

    feat: addTocart y UpdateCart

--- frontend/src/services/cartService.js
+++ frontend/src/services/cartService.js
@@ -4,3 +4,13 @@ export const getCart = async () => {
     const res = await api.get("/cart");
     return res.data;
 }
+
+export const addToCart = async (id_product, quantity = 1) => {
+  const res = await api.post("/cart", { id_product, quantity });
+  return res.data;
+};
+
+export const updateCartItem = async (id, quantity) => {
+    const res = await api.put (`/cart/${id}`, {quantity});
+    return res.data;
+}
