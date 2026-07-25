9917d72b17dbcfa59fee476c5196b4efd4452036
Polsito <jhanpolpb840@gmail.com>
2026-07-25 11:25:12 -0500

    fix: configurar service a postgress con database_urL

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,16 +1,11 @@
-
 const { Pool } = require("pg");
-
 const pool = new Pool({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_NAME,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
 });
 
 pool.connect()
-    .then(() => console.log("Conectado a PostgreSQL"))
-    .catch((err) => console.error("Error al conectarse a PostgreSQL", err));
+  .then(() => console.log("Conectado a PostgreSQL"))
+  .catch((err) => console.error("Error conectando a PostgreSQL:", err));
 
 module.exports = pool;
