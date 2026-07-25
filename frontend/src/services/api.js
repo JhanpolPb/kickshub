d12f270bd2511ebc16a5e87e209e2da2c2768382
Polsito <jhanpolpb840@gmail.com>
2026-07-25 11:38:58 -0500

    fix: actualizar url para frontend en produccion

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios"; 
 const api = axios.create({
-    baseURL: "http://localhost:4000/api", 
+    baseURL: "https://kickshub.onrender.com/api", 
 });
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
