a821613ef9c9fdb1484fe75f65d3aaa53a864874
Polsito <jhanpolpb840@gmail.com>
2026-07-27 15:00:17 -0500

    authMiddleware and adminMiddleware

--- backend/src/routes/productRoutes.js
+++ backend/src/routes/productRoutes.js
@@ -8,12 +8,14 @@ const{
     updateProduct,
     deleteProduct
 } = require("../controllers/productController");
+const adminMiddleware = require("../middleware/adminMiddleware");
+const authMiddleware = require("../middleware/authMiddleware");
 
 router.get("/", getProducts);
 router.get("/:id", getProductById);
-router.post("/",createProduct)
-router.put("/:id", updateProduct);
-router.delete("/:id", deleteProduct);
+router.post("/", authMiddleware, adminMiddleware, createProduct)
+router.put("/:id", authMiddleware, adminMiddleware, updateProduct);
+router.delete("/:id", authMiddleware, adminMiddleware, deleteProduct);
 
 module.exports = router;
 
